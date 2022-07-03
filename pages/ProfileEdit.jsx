a36fc0dafa9de4bb6f5fa8102c7c22772892e3ff
dialaabulkhail <diala.sh.98@gmail.com>
2022-07-03 11:20:28 +0300

    organizing edits

--- pages/ProfileEdit.jsx
+++ pages/ProfileEdit.jsx
@@ -1,117 +1,22 @@
 import React from "react";
-
-import Link from 'next/link'
+import Navbar from "./Navbar";
+import Link from "next/link";
 
 const ProfileEdit = () => {
   return (
     <>
-      <meta charSet="utf-8" />
-      <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <title>Mentor Bootstrap Template - Index</title>
-      <meta content="" name="description" />
-      <meta content="" name="keywords" />
-      {/* Favicons */}
-      <link href="assets/img/favicon.png" rel="icon" />
-      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-      {/* Google Fonts */}
       <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet"
       />
-      {/* Vendor CSS Files */}
-      <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet" />
-      <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
       <link
         href="assets/vendor/bootstrap/css/bootstrap.min.css"
         rel="stylesheet"
       />
-      <link
-        href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-        rel="stylesheet"
-      />
-      <link
-        href="assets/vendor/boxicons/css/boxicons.min.css"
-        rel="stylesheet"
-      />
       <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
-      <link
-        href="assets/vendor/swiper/swiper-bundle.min.css"
-        rel="stylesheet"
-      />
-      {/* Template Main CSS File */}
       <link href="assets/css/style.css" rel="stylesheet" />
 
-      {/* ======= Header ======= */}
-      <header id="header" className="fixed-top">
-        <div className="container d-flex align-items-center">
-          <h1 className="logo me-auto">
-            <a href="/Home">TUTORium</a>
-          </h1>
-
-          <nav id="navbar" className="navbar order-last order-lg-0">
-            <ul>
-              <li>
-                <a className="active" href="/Home">
-                  Home
-                </a>
-              </li>
-
-              <li className="dropdown">
-                <a href="#">
-                  <span>Categories</span> <i className="bi bi-chevron-down" />
-                </a>
-                <ul>
-                  <li>
-                    <a href="/Art">Arts and Crafts</a>
-                  </li>
-
-                  <li>
-                    <a href="#">Technology</a>
-                  </li>
-                  <li>
-                    <a href="#">Health</a>
-                  </li>
-                  <li>
-                    <a href="#">Sports</a>
-                  </li>
-                  <li>
-                    <a href="#">Sciences</a>
-                  </li>
-                  <li>
-                    <a href="#">Business</a>
-                  </li>
-                  <li>
-                    <a href="#">Languages</a>
-                  </li>
-                  <li>
-                    <a href="#">Beauty</a>
-                  </li>
-                  <li>
-                    <a href="#">Cooking</a>
-                  </li>
-                  <li>
-                    <a href="#">Agriculture</a>
-                  </li>
-                  <li>
-                    <a href="#">Music</a>
-                  </li>
-                  <li>
-                    <a href="#">Others</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="/Contact">Contact</a>
-              </li>
-            </ul>
-            <i className="bi bi-list mobile-nav-toggle" />
-          </nav>
-          {/* .navbar */}
-          <a href="/Login" className="get-started-btn">
-            Login
-          </a>
-        </div>
-      </header>
+      <Navbar />
 
       <section className="about" id="profileform">
         <form className="php-email-form">
@@ -243,13 +148,13 @@ const ProfileEdit = () => {
                   </div>
 
                   <div className="mt-5 text-center ">
-                    <Link href='/Profile'>
-                    <button
-                      className="btn btn-primary profile-button"
-                      type="submit"
-                    >
-                      Save Profile
-                    </button>
+                    <Link href="/Profile">
+                      <button
+                        className="btn btn-primary profile-button"
+                        type="submit"
+                      >
+                        Save Profile
+                      </button>
                     </Link>
                   </div>
                 </div>
